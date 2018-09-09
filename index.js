93cf8d849f09c2a43723944dde3dd1b8c175de0c
Jayden Windle <jaydenwindle@gmail.com>
2018-09-08 19:33:07 -0700

    Improved security by implementing IVs, HMAC verification and key derivation using scrypt

--- index.js
+++ index.js
@@ -1,31 +1,54 @@
 const crypto = require('crypto'),
-      envfile = require('envfile')
+      envfile = require('envfile'),
       writeFile = require('fs').writeFileSync;
 
-const ALGORITHM = 'aes-256-cbc';
+const ENCRYPTION_ALGORITHM = 'aes-256-ctr';
+const HMAC_ALGORITHM = 'sha256';
+const AUTHENTICATION_KEY = 'SENV_AUTHENTICATION';
 
 /**
  * Encrypts a string.
  * @param {string} string - The string to be encrypted.
  * @param {string} password - The password with which to encrypt the string.
+ * @param {string} iv - The IV with which to encrypt the string.
  */
-function encryptString(string, password) {
-    var cipher = crypto.createCipher(ALGORITHM, password)
-    var crypted = cipher.update(string, 'utf8', 'hex')
-    crypted += cipher.final('hex');
-    return crypted;
+async function encryptString(string, password, iv) {
+    const key = crypto.scryptSync(password, iv, 32);
+
+    const cipher = crypto.createCipheriv(ENCRYPTION_ALGORITHM, key, iv);
+    let encrypted = cipher.update(string, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+
+    return encrypted;
 }
 
 /**
  * Decrypts a string.
  * @param {string} string - The string to be decrypted.
  * @param {string} password - The password with which to decrypt the string.
+ * @param {string} iv - The IV with which to encrypt the string.
  */
-function decryptString(text, password) {
-    var decipher = crypto.createDecipher(ALGORITHM, password)
-    var dec = decipher.update(text, 'hex', 'utf8')
-    dec += decipher.final('utf8');
-    return dec;
+async function decryptString(string, password, iv) {
+    const key = crypto.scryptSync(password, iv, 32);
+
+    const decipher = crypto.createDecipheriv(ENCRYPTION_ALGORITHM, key, iv);
+    let decrypted = decipher.update(string, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+
+    return decrypted;
+}
+
+/**
+ * Creates an HMAC from a string and password.
+ * @param {string} string - The string to with which to create the HMAC.
+ * @param {string} password - The password with which to create the HMAC.
+ * 
+ * @returns {string}  - The created HMAC.
+ */
+function createHmac(string, password) {
+    hmac = crypto.createHmac(HMAC_ALGORITHM, password);
+    hmac.update(string);
+    return hmac.digest('hex');
 }
 
 /**
@@ -37,7 +60,7 @@ function decryptString(text, password) {
  * @return {string}            - If outputFile is undefined, encrypted .env contents will be
  *                               returned as a string. Otherwise returns success message.
  */
-function encryptEnvFile(inputFile, outputFile, password) {
+async function encryptEnvFile(inputFile, outputFile, password) {
     if (!password) {
         throw new Error(
             'No password provided.'
@@ -45,12 +68,17 @@ function encryptEnvFile(inputFile, outputFile, password) {
     }
     const envVariables = envfile.parseFileSync(inputFile);
 
+    const hmac = createHmac(JSON.stringify(envVariables), password);
+    const iv = hmac.slice(0, 16);
+
     for (const variableName in envVariables) {
         if (envVariables.hasOwnProperty(variableName)) {
             const value = envVariables[variableName];
-            envVariables[variableName] = encryptString(value, password);
+            envVariables[variableName] = await encryptString(value, password, iv);
         }
     }
+
+    envVariables[AUTHENTICATION_KEY] = hmac;
 
     const encryptedEnvVariables = envfile.stringifySync(envVariables);
 
@@ -71,19 +99,29 @@ function encryptEnvFile(inputFile, outputFile, password) {
  * @return {string}            - If outputFile is undefined, encrypted .env contents will be
  *                               returned as a string. Otherwise returns success message.
  */
-function decryptEnvFile(inputFile, outputFile, password) {
+async function decryptEnvFile(inputFile, outputFile, password) {
     if (!password) {
         throw new Error(
             'No password provided.'
         );
     }
     const envVariables = envfile.parseFileSync(inputFile);
+    const hmac = envVariables[AUTHENTICATION_KEY];
+    const iv = hmac.slice(0, 16);
+
+    delete envVariables[AUTHENTICATION_KEY];
 
     for (const variableName in envVariables) {
         if (envVariables.hasOwnProperty(variableName)) {
             const encryptedValue = envVariables[variableName];
-            envVariables[variableName] = decryptString(encryptedValue, password);
+            envVariables[variableName] = await decryptString(encryptedValue, password, iv);
         }
+    }
+
+    const calculatedHmac = createHmac(JSON.stringify(envVariables), password);
+
+    if (hmac !== calculatedHmac) {
+        throw new Error('Incorrect password provided.');
     }
 
     const decryptedEnvVariables = envfile.stringifySync(envVariables);
